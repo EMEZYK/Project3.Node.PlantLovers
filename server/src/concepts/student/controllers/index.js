b3f265bf08adde72b4d5074c6b9fcc59437980a4
Rafał <xbl@gazeta.pl>
2022-02-22 16:07:29 +0100

    add prettier to project

--- server/src/concepts/student/controllers/index.js
+++ server/src/concepts/student/controllers/index.js
@@ -3,14 +3,13 @@ import { createNewStudent } from "../useCases/createNewStudent.js";
 // only controllers, first layer of our app, here is a function which client trigger when send request to our server
 
 export const createStudent = async (req, res) => {
+  // some request validation
 
-    // some request validation
-    
-    try {
-        const newStudent = await createNewStudent();
-        res.send(newStudent);
-    } catch(err) {
-        console.log(err);
-        res.status(500).send("Something went wrong");
-    }
-}
+  try {
+    const newStudent = await createNewStudent();
+    res.send(newStudent);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong");
+  }
+};
--- server/src/concepts/student/repositories/queries.js
+++ server/src/concepts/student/repositories/queries.js
@@ -1,17 +1,16 @@
-import Student from '../model/Student.js';
+import Student from "../model/Student.js";
 
 // Place for queries: get Items from DB
 export async function getStudent(studentId) {
-    const student = await Student.findById(studentId);
-    
-    return student;
+  const student = await Student.findById(studentId);
+
+  return student;
 }
 
 export async function getAllstudents(options) {
+  console.log(options);
 
-    console.log(options);
+  const students = await Student.find();
 
-    const students = await Student.find();
-    
-    return students;
+  return students;
 }
