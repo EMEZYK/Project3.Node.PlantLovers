325a65d24de9cbb3bc12e2bcb4770aadcffea309
kam237zasada <57039178+kam237zasada@users.noreply.github.com>
2022-03-01 12:30:26 +0100

    Change port to give heroku able to bind it
    process.env.PORT would be given after deploy on heroku (it is env on heroku) but locally it would still listen on 3000

--- server/src/index.js
+++ server/src/index.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.SERVER_PORT || 3000, () => {
-  console.log(`Server started at http://localhost:${process.env.SERVER_PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started`);
 });
 
 mongoose
