c7a89a94838f02f1c05d8bf5648b70e4c31980dc
Your Name <donata1989@gmail.com>
2022-03-01 17:27:08 +0100

    Fix: add const instead of let

--- server/src/services/mail/index.js
+++ server/src/services/mail/index.js
@@ -2,31 +2,29 @@ import nodemailer from "nodemailer";
 import { config } from "dotenv";
 config();
 
-const sendEmail = () =>{
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_USER_PASSWORD,
+  },
+});
 
-    let transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_USER_PASSWORD
-        }
-    });
-  
-    let mailOptions = {
-      from: '"Plant Lovers" <plantlovers@plantme.com>',
-      to: "daisy@example.com",
-      text: "Can you adopt me?",
-      subject: "Hello Plant Lover 🌼",
-    };
-  
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent:', info.envelope);   
-  
-    });
-  }
-  
-  export default sendEmail;
+const sendEmail = (data) => {
+  const mailOptions = {
+    from: '"Plant Lovers" <plantlovers@plantme.com>',
+    to: data.to,
+    text: data.text,
+    subject: `${data.subject} 🌼`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent:", info.envelope);
+  });
+};
+
+export default sendEmail;
