bebed7ce8bdefbadca634ef296ef145c9ab04980
Asia Czyżewska <jczyzewska96@gmail.com>
2022-03-21 18:42:12 +0100

    delete location, add forExchange

--- server/src/concepts/offer/useCases/createNewOffer.js
+++ server/src/concepts/offer/useCases/createNewOffer.js
@@ -1,6 +1,4 @@
 import { createOffer } from "../repositories/commands.js";
-import { findCategoryByName } from "../../category/repositories/queries.js";
-import { findHeightByValue } from "../../height/repositories/queries.js";
 
 export async function createNewOffer(body) {
   try {
@@ -8,12 +6,12 @@ export async function createNewOffer(body) {
       title: body.title,
       description: body.description,
       city: body.city,
-      location: body.location || {},
       phoneNumber: body.phoneNumber,
       photos: body.photos,
-      category: getCategoryId(body.category),
-      height: getHeightId(body.height),
+      category: body.category,
+      height: body.height,
       price: body.price,
+      forExchange: body.forExchange,
     });
 
     return offer;
@@ -21,14 +19,4 @@ export async function createNewOffer(body) {
     console.log(err);
     return new Error("Offer wasn't created");
   }
-}
-
-async function getCategoryId(categoryName) {
-  const category = await findCategoryByName(categoryName);
-  return category._id;
-}
-
-async function getHeightId(heightRange) {
-  const height = await findHeightByValue(heightRange);
-  return height._id;
 }
