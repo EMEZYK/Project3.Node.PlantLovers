c4b80871125b5ab8fb375e3e057662117826aa72
Ewelina Mężyk <ewelinamezykmail@gmail.com>
2022-03-21 12:13:03 +0000

    Create emails templates

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,6 @@ import usersRoutes from "./concepts/user/routes/index.js";
 import categoryRoutes from "./concepts/category/routes/index.js";
 import heightRoutes from "./concepts/height/routes/index.js";
 
-
 if (!process.env.MONGO_CONNECT_URI)
   throw new Error("You must provide uri for mongo connect in env.");
 
