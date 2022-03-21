a0c0116ecb99063bf12a393a5bc298c659e0365f
Mateusz <mateusz.wyborski@coderscrew.pl>
2022-03-21 19:16:34 +0100

    add getOffers useCase, remove validation, fix small changes

--- server/src/concepts/offer/controllers/index.js
+++ server/src/concepts/offer/controllers/index.js
@@ -1,17 +1,9 @@
-import OfferValidation from "../model/OfferValidation.js";
 import { getOffers } from "../useCases/getOffers.js";
 
-export const getOffersHere = async (req, res) => {
-  const validationResult = OfferValidation(req.body);
-  if (validationResult.error) {
-    return res.status(400).send("invalid data");
-  }
+export const getAllOffers = async (req, res) => {
   try {
     const offers = await getOffers(req.body);
-    return res.status(200).send({
-      message: "Offers were collected",
-      data: offers,
-    });
+    return res.status(200).send(offers);
   } catch (error) {
     return res.status(500).send(error.message);
   }
--- server/src/concepts/offer/useCases/getOffers.js
+++ server/src/concepts/offer/useCases/getOffers.js
@@ -1,7 +1,3 @@
-// not sure if i need to create command for getOffers
-// import {  } from "../repositories/commands.js"
+import { getAllOffers } from "../repositories/queries.js";
 
-// this query is ready
-// import { getAllOffers } from "../repositories/queries.js";
-
-// and here is problem, not sure how to create this useCase for getOffers
+getAllOffers({}, 10, 10);
